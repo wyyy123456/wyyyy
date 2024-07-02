56b8ea91dab90d60ce4e9d92d00f63294558daa7
wyyy123456 <167738602+wyyy123456@users.noreply.github.com>
2024-07-02 22:05:00 +0800

    粒子系统

--- Assets/script/LeverToPrint.cs
+++ Assets/script/LeverToPrint.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class LeverToPrint : MonoBehaviour
 {
     public Animator anim;
+    public GameObject particlePrefab;
+    public Transform spawnLocation;
+
 
     public void PlayPrintAnim()
     {
         anim.SetTrigger("Print");
-        //play vfx
+        InstantiateAndPlayParticleSystem();
+         //play vfx
         //Instantiate(xxx);
         //play sfx
         //xxx.PlayOneShot();
     }
+    public void InstantiateAndPlayParticleSystem()
+    {
+        if (particlePrefab != null && spawnLocation != null)
+        {
+            // 实例化粒子系统
+            GameObject particleInstance = Instantiate(particlePrefab, spawnLocation.position, spawnLocation.rotation);
+
+            // 获取并播放粒子系统
+            ParticleSystem particleSystem = particleInstance.GetComponent<ParticleSystem>();
+            if (particleSystem != null)
+            {
+                particleSystem.Play();
+            }
+
+        }
+    }
+   
 }
